0defe7aa355f904bac678f659ae523115a62b630
jamie-codez <cruiseomondi90@gmail.com>
2023-05-19 20:27:14 +0300

    Creating the input forms

--- app/src/main/java/com/example/composetut/ui/views/Authentication.kt
+++ app/src/main/java/com/example/composetut/ui/views/Authentication.kt
@@ -1,12 +1,26 @@
 package com.example.composetut.ui.views
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.composetut.R
+import com.example.composetut.data.AuthenticationMode
 import com.example.composetut.data.AuthenticationState
 import com.example.composetut.vm.AuthenticationViewModel
 import com.example.composetut.vm.actions.AuthenticationEvent
@@ -29,5 +43,35 @@ fun AuthenticationContent(
     authenticationState: AuthenticationState,
     handleEvent: (event: AuthenticationEvent) -> Unit
 ) {
+    Box(modifier = modifier, contentAlignment = Alignment.Center) {
+        if (authenticationState.isLoading) {
+            CircularProgressIndicator()
+        } else {
+            AuthenticationForm(
+                modifier = Modifier.fillMaxSize(),
+                authenticationMode = authenticationState.authenticationMode
+            )
+        }
+    }
+}
 
+@Composable
+fun AuthenticationForm(modifier: Modifier = Modifier, authenticationMode: AuthenticationMode) {
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        Spacer(modifier = Modifier.height(32.dp))
+        AuthenticationTitle(authenticationMode = authenticationMode)
+    }
+}
+
+@Composable
+fun AuthenticationTitle(modifier: Modifier = Modifier, authenticationMode: AuthenticationMode) {
+    Text(
+        text = stringResource(
+            if (authenticationMode == AuthenticationMode.SIGN_IN) {
+                R.string.label_sign_in_to_account
+            } else {
+                R.string.label_sign_up_for_account
+            }
+        ), fontSize = 24.sp, fontWeight = FontWeight.Black
+    )
 }
